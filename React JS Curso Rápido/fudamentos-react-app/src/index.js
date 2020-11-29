3f45701e0842a99e647f91e2dd5d3f07b465a333
Messias Oliveira <messias.oliveira6296@gmail.com>
2020-11-28 21:19:48 -0300

    example of first react component

---
+++ React JS Curso Rápido/fudamentos-react-app/src/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Primeiro from './components/primeiro'
+
+ReactDOM.render(
+    <Primeiro />,
+    document.getElementById("root")
+)
